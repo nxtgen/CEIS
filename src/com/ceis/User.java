427760f0510521d37939f644dc8509d54ff79d64
kartkris <kartik.vedula@gmail.com>
2013-06-02 12:35:14 +0530

    User java bean initial commit

---
+++ src/com/ceis/User.java
@@ -0,0 +1,45 @@
+/*
+ * User bean class
+ * 
+ */
+package ceis;
+
+import java.io.Serializable;
+
+public class User implements Serializable
+{
+    private String id;
+    private String passwd;
+    
+    public User()
+    {
+        id = "";
+        passwd = "";
+    }
+            
+    public User (String id, String passwd)
+    {
+        this.id = id;
+        this.passwd = passwd;
+    }
+    
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getId()
+    { 
+        return id; 
+    }
+    
+    public void setPassWd(String passwd)
+    {
+        this.passwd = passwd;
+    }
+
+    public String getPassWd()
+    { 
+        return passwd; 
+    }
+}
